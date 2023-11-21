c9896a25090c661f5ec36c737df583c12edc84a9
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-21 12:53:32 +0530

    a form aware drop down button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppinglistapp/data/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -23,13 +24,46 @@ class _NewItemState extends State<NewItem> {
               decoration: InputDecoration(
                 label: Text('Name'),
               ),
-              validator: (value){
-                if(value == null || value.isEmpty){
+              validator: (value) {
+                if (value == null || value.isEmpty) {
                   return 'Please enter a name';
                 }
-                return null;
+                return 'Demo..';
               },
             ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for (final category in categories.entries)
+                      DropdownMenuItem(
+                        value: category.value,
+                        child: Row(children: [
+                          Container(
+                            width: 16,
+                            height: 16,
+                            color: category.value.color,
+                          ),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          Text(category.value.title),
+                        ]),
+                      )
+                  ], onChanged: (value) {}),
+                )
+              ],
+            )
           ],
         ),
       ),
