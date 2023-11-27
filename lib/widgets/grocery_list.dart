2c362c852de65303a6473f74ba1a1f1940f6430e
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-27 12:36:01 +0530

    better way of error handling by using try and catch

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -28,32 +28,54 @@ class _GroceryListState extends State<GroceryList> {
   void _loadItems() async {
     final url = Uri.https(
         'flutter-prep-845ac-default-rtdb.firebaseio.com', 'shopping-list.json');
-    final response = await http.get(url);
-    if (response.statusCode >= 400) {
+
+try {
+     final response = await http.get(url);
+      if (response.statusCode >= 400) {
+     
+      } // >400 = error catch block take out that set state 
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+       final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
       setState(() {
-        _error = 'Failed to fetch data .Please try again later .';
+        _groceryItems = loadedItems;
+        _isLoading = false;
       });
-    } // >400 = error
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
-      loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ),
-      );
-    }
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
+}catch (error){
+      setState(() {
+        _error = 'Something went Wrong ! Please try again later .';
+      });
+
+}
+
+ 
+
+
+
+   
+
+  
   }
 
   void _addItem() async {
@@ -83,11 +105,10 @@ class _GroceryListState extends State<GroceryList> {
     final response = await http.delete(url);
     if (response.statusCode >= 400) {
       //Optional error
-       setState(() {
-        _groceryItems.insert(index,item);
+      setState(() {
+        _groceryItems.insert(index, item);
       });
     }
-   
   }
 
   @override
